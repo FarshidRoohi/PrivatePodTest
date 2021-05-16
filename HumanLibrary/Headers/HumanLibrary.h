ea0344578462800ff4633abee74bde8a9a546d85
Farshidroohi <farshid.roohi.a@gmail.com>
2021-05-16 11:19:53 +0430

    update

---
+++ HumanLibrary/Headers/HumanLibrary.h
@@ -0,0 +1,18 @@
+//
+//  HumanLibrary.h
+//  HumanLibrary
+//
+//  Created by Farshid on 5/15/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HumanLibrary.
+FOUNDATION_EXPORT double HumanLibraryVersionNumber;
+
+//! Project version string for HumanLibrary.
+FOUNDATION_EXPORT const unsigned char HumanLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HumanLibrary/PublicHeader.h>
+
+
